2eb07b9898e42e8bc8a83a4ae6b94081c4e60913
manish-singh-codes <manishsinghcodes@gmail.com>
2025-06-28 23:43:50 +0530

    add shadcn

---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import React from 'react'
+
+interface NavbarProps {
+
+    // Define any props if needed
+    }
+
+const Navbar: React.FC<NavbarProps> =  () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
